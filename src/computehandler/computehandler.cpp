330c1453b0e778bf29184ea8bbaa18f2266d1574
Bzhouha <bzhouha@outlook.com>
2025-04-02 23:58:41 +0800

    测试CMake

---
+++ src/computehandler/computehandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 毕洲华 on 25-4-2.
+//
+
+#include "computehandler.h"
---
+++ src/computehandler/computehandler.h
@@ -0,0 +1,24 @@
+//
+// Created by 毕洲华 on 25-4-2.
+//
+
+#ifndef COMPUTEHANDLER_H
+#define COMPUTEHANDLER_H
+
+
+
+class computehandler {
+public:
+  computehandler();
+  ~computehandler();
+  void chshow();
+  void compute();
+  private:
+    // parameter
+    // input filename
+    // output filename
+};
+
+
+
+#endif //COMPUTEHANDLER_H
